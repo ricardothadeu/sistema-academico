64eb19eda345e219777c21ab54e9ee671d9d768f
Ricardo Matos <ricardothadeu8@outlook.com>
2022-04-15 23:51:33 -0300

    Add models

--- academico/admin.py
+++ academico/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Aluno, Disciplina, Professor, Curso
 
-# Register your models here.
+@admin.register(Aluno)
+class AlunoAdmin(admin.ModelAdmin):
+    list_display = ('nome_completo', 'matricula')
+
+@admin.register(Curso)
+class CursoAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Disciplina)
+class DisciplinaAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Professor)
+class ProfessorAdmin(admin.ModelAdmin):
+    pass
--- academico/models.py
+++ academico/models.py
@@ -1,3 +1,40 @@
+import uuid
 from django.db import models
 
-# Create your models here.
+class Disciplina (models.Model):
+    nome = models.CharField(max_length=30)
+    
+    def __str__(self):
+        return self.nome
+
+class Curso (models.Model):
+    nome = models.CharField(max_length=40)
+
+    def __str__(self):
+        return self.nome
+
+class Aluno (models.Model):
+    nome_completo = models.CharField(max_length=60)
+    matricula = models.CharField(max_length=12, unique=True)
+    telefone = models.CharField(max_length=12)
+    curso = models.ForeignKey(Curso, on_delete=models.SET_NULL, null=True)
+    email = models.EmailField(verbose_name='e-mail')
+    disciplinas = models.ManyToManyField(Disciplina)
+
+
+    def __str__(self):
+        return f'{self.matricula}'
+
+class Professor (models.Model):
+    nome_completo = models.CharField(max_length=60)
+    matricula = models.CharField(max_length=12, unique=True)
+    telefone = models.CharField(max_length=12)
+    email = models.EmailField(verbose_name='e-mail')
+    disciplinas = models.ForeignKey('Disciplina', on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return f'{self.matricula}'
+    
+
+
+
